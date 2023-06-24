3fe18a0f4b8bbcfd6a3f4b63e751201ce9b8eb88
Kemosabe2911 <stevinprince29@gmail.com>
2023-06-24 19:25:23 +0530

    test post message in slack channel

--- bot.py
+++ bot.py
@@ -8,3 +8,5 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+
+client.chat_postMessage(channel='#test', text="Hello World!")
