b3b4da4ada4a9e418be9782816e4e45970e55a6d
Kemosabe2911 <stevinprince29@gmail.com>
2023-07-08 17:11:38 +0530

    events setup

--- bot.py
+++ bot.py
@@ -2,11 +2,19 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
 
 # set env path
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
+
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
 client.chat_postMessage(channel='#test', text="Hello World!")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
