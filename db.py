bc222368a4f7bc244f8c41cf6715373e111337e2
Kemosabe2911 <stevinprince29@gmail.com>
2023-07-15 17:05:54 +0530

    init db

---
+++ db.py
@@ -0,0 +1,40 @@
+from mongoengine import *
+from dotenv import load_dotenv
+from pathlib import Path
+
+# set env path
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+# connec to db
+connect(host=os.environ['DB_URI'])
+
+class PersonalLinks(Document):
+    user = StringField(required=True)
+    channel = StringField(required=True)
+    source = StringField(required=True)
+    link = StringField(required=True)
+
+
+def InsertPersonalLinks(user, channel, source, link):
+    personalLink = PersonalLinks(
+        user=user,
+        channel=channel,
+        source=source,
+        link=link
+    )
+    personalLink.save()
+
+def FetchPersonalLinks(user, channel):
+    personalLinksArray = []
+    for personalLink in PersonalLinks.objects(user=user, channel=channel)
+        data = PersonalLinks(
+            personalLink.user, 
+            personalLink.channel, 
+            personalLink.source, 
+            personalLink.link
+        )
+        personalLinksArray.append(data)
+
+    return personalLinksArray if len(personalLinksArray) > 0 else None
---
+++ models/PersonalLinks.py
@@ -0,0 +1,6 @@
+class PersonalLinks:
+    def __init__(self, user, channel, source, link):
+        self.user = user
+        self.channel = channel
+        self.source = source
+        self.link = link
